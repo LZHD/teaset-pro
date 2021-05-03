b8109d95e16efd167c1fb3c8f78974b604a4824b
LZHD <lzhdfxq@163.com>
2021-05-03 23:35:54 +0800

    style: modify code style for component

--- components/ActionSheet/ActionSheet.js
+++ components/ActionSheet/ActionSheet.js
@@ -1,26 +1,17 @@
-// ActionSheet.js
-
-'use strict';
-
-import React, {Component} from "react";
-import {View} from 'react-native';
-
+import React from 'react';
 import Overlay from '../Overlay/Overlay';
 import ActionSheetView from './ActionSheetView';
 
 export default class ActionSheet extends Overlay {
-
   static ActionSheetView = ActionSheetView;
 
-  // items and cancelItem shape
-  //   title: PropTypes.string.isRequired,
-  //   onPress: PropTypes.func,
-  //   disabled: PropTypes.bool,
   static show(items, cancelItem, options = {}) {
     return super.show(
-      <this.ActionSheetView items={items} cancelItem={cancelItem} {...options} />
+      <this.ActionSheetView
+        items={items}
+        cancelItem={cancelItem}
+        {...options}
+      />,
     );
   }
-
 }
-
