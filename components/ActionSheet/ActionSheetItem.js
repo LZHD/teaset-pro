5391848e31257374b159d428a6bb06cc5d505abd
LZHD <lzhdfxq@163.com>
2021-05-03 23:35:54 +0800

    style: modify code style for component

--- components/ActionSheet/ActionSheetItem.js
+++ components/ActionSheet/ActionSheetItem.js
@@ -1,21 +1,25 @@
-// ActionSheetItem.js
-
-'use strict';
-
-import React, {Component} from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
-
+import { View, Text, TouchableOpacity } from 'react-native';
 import Theme from '../../themes/Theme';
 
 export default class ActionSheetItem extends Component {
-
   static propTypes = {
     ...TouchableOpacity.propTypes,
     type: PropTypes.oneOf(['default', 'cancel']),
-    title: PropTypes.oneOfType([PropTypes.element, PropTypes.string, PropTypes.number]),
-    topSeparator: PropTypes.oneOfType([PropTypes.element, PropTypes.oneOf(['none', 'full', 'indent'])]),
-    bottomSeparator: PropTypes.oneOfType([PropTypes.element, PropTypes.oneOf(['none', 'full', 'indent'])]),
+    title: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.string,
+      PropTypes.number,
+    ]),
+    topSeparator: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.oneOf(['none', 'full', 'indent']),
+    ]),
+    bottomSeparator: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.oneOf(['none', 'full', 'indent']),
+    ]),
     disabled: PropTypes.bool,
   };
 
@@ -28,50 +32,63 @@ export default class ActionSheetItem extends Component {
   };
 
   buildStyle() {
-    let {style, type} = this.props;
-    style = [{
-      backgroundColor: type === 'cancel' ? Theme.asCancelItemColor : Theme.asItemColor,
-      paddingLeft: Theme.asItemPaddingLeft,
-      paddingRight: Theme.asItemPaddingRight,
-      paddingTop: Theme.asItemPaddingTop,
-      paddingBottom: Theme.asItemPaddingBottom,
-      minHeight: Theme.asItemMinHeight,
-      overflow: 'hidden',
-      justifyContent: 'center',
-    }].concat(style);
+    let { style, type } = this.props;
+    style = [
+      {
+        backgroundColor:
+          type === 'cancel' ? Theme.asCancelItemColor : Theme.asItemColor,
+        paddingLeft: Theme.asItemPaddingLeft,
+        paddingRight: Theme.asItemPaddingRight,
+        paddingTop: Theme.asItemPaddingTop,
+        paddingBottom: Theme.asItemPaddingBottom,
+        minHeight: Theme.asItemMinHeight,
+        overflow: 'hidden',
+        justifyContent: 'center',
+      },
+    ].concat(style);
     return style;
   }
 
   renderSeparator(separator) {
-    let {type} = this.props;
+    let { type } = this.props;
 
     let indentViewStyle = {
       backgroundColor: 'rgba(0,0,0,0)',
       paddingLeft: Theme.asItemPaddingLeft,
-    }
+    };
     let separatorStyle;
     if (type === 'cancel') {
       separatorStyle = {
         backgroundColor: Theme.asCancelItemSeparatorColor,
-        height: Theme.asCancelItemSeparatorLineWidth,
-      }
+        height: +Theme.asCancelItemSeparatorLineWidth,
+      };
     } else {
       separatorStyle = {
         backgroundColor: Theme.asItemSeparatorColor,
-        height:Theme.asItemSeparatorLineWidth,
-      }
+        height: +Theme.asItemSeparatorLineWidth,
+      };
     }
     switch (separator) {
-      case 'full': return <View style={separatorStyle} />;
-      case 'indent': return <View style={indentViewStyle}><View style={separatorStyle} /></View>;
-      case 'none': return null;
-      default: return React.isValidElement(separator) ? separator : null;
+      case 'full':
+        return <View style={separatorStyle} />;
+      case 'indent':
+        return (
+          <View style={indentViewStyle}>
+            <View style={separatorStyle} />
+          </View>
+        );
+      case 'none':
+        return null;
+      default:
+        return React.isValidElement(separator) ? separator : null;
     }
   }
 
   renderTitle() {
-    let {type, title, disabled} = this.props;
-    if (title === null || title === undefined || React.isValidElement(title)) return title;
+    let { type, title, disabled } = this.props;
+    if (title === null || title === undefined || React.isValidElement(title)) {
+      return title;
+    }
 
     let textStyle;
     if (type === 'cancel') {
@@ -93,21 +110,36 @@ export default class ActionSheetItem extends Component {
         overflow: 'hidden',
       };
     }
-    return <Text style={textStyle} numberOfLines={1}>{title}</Text>;
+    return (
+      <Text style={textStyle} numberOfLines={1}>
+        {title}
+      </Text>
+    );
   }
 
   render() {
-    let {style, children, type, title, topSeparator, bottomSeparator, disabled, activeOpacity, ...others} = this.props;
+    let {
+      style,
+      children,
+      type,
+      title,
+      topSeparator,
+      bottomSeparator,
+      disabled,
+      activeOpacity,
+      ...others
+    } = this.props;
     return (
-      <View style={{backgroundColor: 'rgba(0, 0, 0, 0)'}} >
+      <View style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}>
         {this.renderSeparator(topSeparator)}
-        <TouchableOpacity style={this.buildStyle()} activeOpacity={disabled ? 1 : activeOpacity} {...others}>
+        <TouchableOpacity
+          style={this.buildStyle()}
+          activeOpacity={disabled ? 1 : activeOpacity}
+          {...others}>
           {this.renderTitle()}
         </TouchableOpacity>
         {this.renderSeparator(bottomSeparator)}
       </View>
     );
   }
-
 }
-
--- components/ActionSheet/ActionSheetView.js
+++ components/ActionSheet/ActionSheetView.js
@@ -1,26 +1,30 @@
-// ActionSheetView.js
-
-'use strict';
-
-import React, {Component} from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import {View} from 'react-native';
-
+import { View } from 'react-native';
 import Theme from '../../themes/Theme';
 import Overlay from '../Overlay/Overlay';
 import ActionSheetItem from './ActionSheetItem';
 
 export default class ActionSheetView extends Overlay.PullView {
-
   static propTypes = {
     ...Overlay.PullView.propTypes,
-    items: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.oneOfType([PropTypes.element, PropTypes.string, PropTypes.number]).isRequired,
-      onPress: PropTypes.func,
-      disabled: PropTypes.bool,
-    })),
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.oneOfType([
+          PropTypes.element,
+          PropTypes.string,
+          PropTypes.number,
+        ]).isRequired,
+        onPress: PropTypes.func,
+        disabled: PropTypes.bool,
+      }),
+    ),
     cancelItem: PropTypes.shape({
-      title: PropTypes.oneOfType([PropTypes.element, PropTypes.string, PropTypes.number]).isRequired,
+      title: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.string,
+        PropTypes.number,
+      ]).isRequired,
       onPress: PropTypes.func,
       disabled: PropTypes.bool,
     }),
@@ -34,20 +38,24 @@ export default class ActionSheetView extends Overlay.PullView {
   }
 
   onItemPress(item) {
-    if (item && item.disabled) return;
+    if (item && item.disabled) {
+      return;
+    }
     this.pressItem = item;
     this.close(false);
   }
 
   onCancelItemPress() {
-    let {cancelItem} = this.props;
-    if (cancelItem && cancelItem.disabled) return;
+    let { cancelItem } = this.props;
+    if (cancelItem && cancelItem.disabled) {
+      return;
+    }
     this.pressItem = cancelItem;
     this.close(true);
   }
 
   renderContent() {
-    let {items, cancelItem} = this.props;
+    let { items, cancelItem } = this.props;
 
     let list = [];
     for (let i = 0; items && i < items.length; ++i) {
@@ -59,33 +67,33 @@ export default class ActionSheetView extends Overlay.PullView {
           topSeparator={i === 0 ? 'none' : 'full'}
           disabled={item.disabled}
           onPress={() => this.onItemPress(item)}
-          />
+        />,
       );
     }
     if (cancelItem) {
       list.push(
         <this.constructor.Item
           key={'cancelItem'}
-          type='cancel'
+          type="cancel"
           title={cancelItem.title}
-          topSeparator='full'
+          topSeparator="full"
           disabled={cancelItem.disabled}
           onPress={() => this.onCancelItemPress()}
-          />
+        />,
       );
     }
     list.push(
       <View
         style={{
-          backgroundColor: cancelItem ? Theme.asCancelItemColor : Theme.asItemColor,
-          height: Theme.screenInset.bottom
+          backgroundColor: cancelItem
+            ? Theme.asCancelItemColor
+            : Theme.asItemColor,
+          height: Theme.screenInset.bottom,
         }}
         key={'bottomSpace'}
-        />
+      />,
     );
 
     return super.renderContent(list);
   }
-
 }
-
