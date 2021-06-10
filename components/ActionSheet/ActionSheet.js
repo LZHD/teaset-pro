7dfbf43b0296eda953f2524dc47d9ad302e4681b
LZHD <lzhdfxq@163.com>
2021-06-10 23:44:08 +0800

    chore: update description for actionsheet

--- components/ActionSheet/ActionSheet.js
+++ components/ActionSheet/ActionSheet.js
@@ -5,6 +5,10 @@ import ActionSheetView from './ActionSheetView';
 export default class ActionSheet extends Overlay {
   static ActionSheetView = ActionSheetView;
 
+  // items and cancelItem shape
+  //   title: PropTypes.string.isRequired,
+  //   onPress: PropTypes.func,
+  //   disabled: PropTypes.bool,
   static show(items, cancelItem, options = {}) {
     return super.show(
       <this.ActionSheetView
